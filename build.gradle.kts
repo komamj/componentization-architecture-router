d91fc5ebafe65da2bc1f1ecf8f8fb6f8a74d90d5
komamj <junmaokoma@gmail.com>
2020-12-16 14:44:15 +0800

    feat: [app] #N/A  add module.

---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("com.alibaba.arouter")
+    id("maven-publish")
+}
+
+kapt {
+    correctErrorTypes = true
+    useBuildCache = true
+    arguments {
+        arg("AROUTER_MODULE_NAME", project.name)
+        arg("AROUTER_GENERATE_DOC", "enable")
+    }
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.2")
+
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+
+            isTestCoverageEnabled = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "consumer-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(Dependencies.Kotlin.STDLIB)
+    implementation(Dependencies.AndroidX.APPCOMPAT)
+    implementation(Dependencies.Others.GSON)
+    api(Dependencies.Others.ROUTER)
+    kapt(Dependencies.Others.ROUTER_COMPILER)
+
+    implementation(project(path = ":platform-log"))
+
+    testImplementation(Dependencies.JunitTest.JUNIT)
+    testImplementation(Dependencies.JunitTest.TRUTH)
+    androidTestImplementation(Dependencies.AndroidTest.JUNIT)
+    androidTestImplementation(Dependencies.JunitTest.MOCK_CORE)
+    androidTestImplementation(Dependencies.JunitTest.TRUTH)
+    androidTestImplementation(Dependencies.AndroidTest.ESPRESSO_CORE)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components.findByName("release"))
+                groupId = "com.github.komamj"
+                artifactId = "platform-router"
+                version = "0.0.1"
+            }
+        }
+    }
+}
